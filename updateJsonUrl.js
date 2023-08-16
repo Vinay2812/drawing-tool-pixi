dba791d9c9b3c59670582d206443afdf6182d504
thebishaldeb <bishaldeb656@gmail.com>
2023-08-16 19:42:33 +0530

    ref: script to replace hash with url

---
+++ updateJsonUrl.js
@@ -0,0 +1,141 @@
+// CONSTANTS
+const ACCESS_TOKEN = "figd_2IJungaIDY7IDq0MjWDJit5hur_N5g3F9mfj7pq1";
+const FILE_KEY = "ixE1TVyHYZObrzeNW1wvrD";
+const BUCKET_NAME = "sets-gamify-assets";
+const FILE_PATH = "dev/figma/assets/";
+const S3_PRESIGN_URL =
+  "https://qjbn7p56fd.execute-api.ap-south-1.amazonaws.com/production/generate";
+
+// JSON DATA
+const jsonData = {};
+
+const fetchAssetsUrls = async () => {
+  const response = await fetch(
+    `https://api.figma.com/v1/files/${FILE_KEY}/images`,
+    {
+      headers: {
+        "X-FIGMA-TOKEN": ACCESS_TOKEN,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data?.meta?.images;
+};
+
+const getPresignedURL = async (filename) => {
+  const bucket_name = BUCKET_NAME;
+  const url = S3_PRESIGN_URL;
+
+  const data = {
+    bucket_name: bucket_name,
+    key: FILE_PATH + filename,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url, options);
+  const resObj = await response.json();
+
+  return resObj;
+};
+
+const CDN_URL = `https://cdn.homeworkapp.ai/${BUCKET_NAME}`;
+const BUCKET_URL = `https://${BUCKET_NAME}.s3.ap-south-1.amazonaws.com`;
+const replaceUrl = (url) => {
+  return url.replace(CDN_URL, BUCKET_URL);
+};
+
+const uploadToS3 = async (imageMap) => {
+  const updatedImageMap = {};
+
+  const imgKeys = imageMap ? Object.keys(imageMap) : [];
+
+  if (imgKeys.length > 0) {
+    // Create an array of promises for concurrent uploads
+    const uploadPromises = imgKeys.map(async (element, i) => {
+      const filename = element;
+
+      try {
+        const response = await fetch(imageMap[element]);
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch file from URL: ${imageMap[element]}`
+          );
+        }
+
+        const fileBlob = await response.blob();
+        const contentType = fileBlob.type;
+
+        const { url, bucket_url } = await getPresignedURL(filename);
+
+        const headers = new Headers();
+        headers.append("Content-Type", contentType);
+
+        const uploadResponse = await fetch(url, {
+          method: "PUT",
+          headers,
+          body: fileBlob,
+        });
+
+        if (uploadResponse.ok) {
+          const uploadedImageUrl = replaceUrl(bucket_url);
+          console.log("File uploaded successfully: ", uploadedImageUrl);
+
+          updatedImageMap[element] = {
+            type: contentType,
+            url: uploadedImageUrl,
+          };
+        } else {
+          console.log("Failed to upload file", uploadResponse);
+        }
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    });
+
+    // Wait for all promises to complete
+    await Promise.all(uploadPromises);
+  }
+
+  return updatedImageMap;
+};
+
+const replaceImageHashes = (obj, imageData) => {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => replaceImageHashes(item, imageData));
+  } else if (obj && typeof obj === "object") {
+    if (obj.imageHash) {
+      const imageHash = obj.imageHash;
+      if (imageData[imageHash]) {
+        if (imageData[imageHash]?.type === "image/gif")
+          obj.gifRef = imageData[imageHash].url;
+        else obj.imageRef = imageData[imageHash].url;
+      }
+    }
+    for (const key in obj) {
+      obj[key] = replaceImageHashes(obj[key], imageData);
+    }
+  }
+  return obj;
+};
+
+const updateFigmaJson = async () => {
+  const imageMap = await fetchAssetsUrls();
+  const updatedImageMap = await uploadToS3(imageMap);
+  console.log("Uploaded Files: ", updatedImageMap);
+
+  const updatedJson = replaceImageHashes(jsonData, updatedImageMap);
+
+  console.log("Updated Json: ", updatedJson);
+  return updatedJson;
+};
+
+updateFigmaJson();
