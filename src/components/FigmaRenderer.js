116f4e1ccd33bee0b0207d783736c9a579828016
r-kohale9 <r.kohale9@gmail.com>
2023-08-17 19:27:46 +0530

    ref: integrate update variable and variant onclick

--- src/components/FigmaRenderer.js
+++ src/components/FigmaRenderer.js
@@ -1,23 +1,45 @@
 // src/components/FigmaRenderer.js
 
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-import { renderFigmaJson } from "../renderer";
+import * as PIXI from 'pixi.js';
+import { parseFigmaJson } from '../parser/parser';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { renderFigmaJson } from '../renderer';
+
+// Create a PIXI Application
+const app = new PIXI.Application({
+  //   width: 1800,
+  //   height: 1800,
+  backgroundColor: 'red',
+  resizeTo: window
+});
 
 const FigmaRenderer = ({ figmaJson }) => {
-	// Create a unique ID for the container element
-	const elementId = "figma-canvas-container";
+  const [figmaJsonState, setFigmaJson] = React.useState(figmaJson);
+  const [isUpdated, setIsUpdated] = React.useState(false);
+  // Create a unique ID for the container element
+  const elementId = 'figma-canvas-container';
 
-	useEffect(() => {
-		// Render the Figma JSON inside the container element
-		renderFigmaJson(figmaJson, elementId);
-	}, []);
+  useEffect(() => {
+    // Render the Figma JSON inside the container element
+    renderFigmaJson(
+      app,
+      figmaJsonState,
+      elementId,
+      e => {
+        setIsUpdated(true);
+        setFigmaJson(e);
+      },
+      isUpdated,
+      setIsUpdated
+    );
+  }, [figmaJsonState, isUpdated]);
 
-	return <div id={elementId} />;
+  return <div id={elementId} />;
 };
 
 FigmaRenderer.propTypes = {
-	figmaJson: PropTypes.object.isRequired,
+  figmaJson: PropTypes.object.isRequired
 };
 
 export default FigmaRenderer;
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,27 +1,17 @@
 // src/renderer/index.js
-
-import * as PIXI from "pixi.js";
-import { parseFigmaJson } from "../parser/parser";
-import { renderFigmaFromParsedJson } from "./renderer";
+import * as PIXI from 'pixi.js';
+import { parseFigmaJson } from '../parser/parser';
+import { renderFigmaFromParsedJson } from './renderer';
 
 // Function to render the Figma JSON using PIXI.js
-export const renderFigmaJson = (figmaJson, elementId) => {
+export const renderFigmaJson = (app, figmaJson, elementId, setFigmaJson, isUpdated, setIsUpdated) => {
   // Check if canvas already exists
   const currentElement = document.getElementById(elementId);
-  const canvas = currentElement?.querySelector("canvas");
+  const canvas = currentElement?.querySelector('canvas');
+
   if (!canvas) {
-    // Parse the Figma JSON into a PIXI Container
-    const parsedJson = parseFigmaJson(figmaJson);
-    const container = renderFigmaFromParsedJson(parsedJson.children);
-
-    // Create a PIXI Application
-    const app = new PIXI.Application({
-      //   width: 1800,
-      //   height: 1800,
-      backgroundColor: "red",
-      resizeTo: window,
-    });
-
+    const parsedJson = !figmaJson.variables ? parseFigmaJson(figmaJson) : figmaJson;
+    const container = renderFigmaFromParsedJson(parsedJson, setFigmaJson);
     // Append the PIXI view to the specified HTML element
     document.getElementById(elementId).appendChild(app.view);
     // Add the container to the PIXI stage
@@ -39,5 +29,15 @@ export const renderFigmaJson = (figmaJson, elementId) => {
     // debugGraphics.drawRect(0, 0, 800, 600);
     // container.addChild(debugGraphics);
     // Optionally, you can add code here to set up interactivity, animations, etc.
+  }
+
+  if (canvas && isUpdated) {
+    if (app.stage.children.length) app.stage.removeChild(app.stage.children[0]);
+
+    const parsedJson = figmaJson;
+    const container = renderFigmaFromParsedJson(parsedJson, setFigmaJson);
+    // console.log('called');
+    app.stage.addChild(container);
+    setIsUpdated(false);
   }
 };
